997d1c6aae31b01ac754742f96fe2746acfa45ce
Oleg Niziev <olegpro23@mail.ru>
2026-03-08 15:21:36 +0300

    Добавление base опции для github pages

--- src/context/analityc.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react';
-type AnalyticState = {
-	yandexId?: number;
-	googleId?: string;
-};
-
-export const Analytic = createContext<AnalyticState>({});
-
-Analytic.displayName = 'AnalyticContext';
